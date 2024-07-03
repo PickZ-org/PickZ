af16440d946e5fe1b2c8e0f6cbebf006faecc81e
Sjoerd Handofsky <sjoerd@webswing.nl>
2024-07-03 16:13:43 +0200

    Changed year to current

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,6 +3,6 @@
     <div class="float-right d-none d-sm-inline">
 
     </div>
-    Copyright &copy; 2023 PickZ contributors. All rights reserved
+    Copyright &copy; {{ now()->year }} PickZ contributors. All rights reserved
 </footer>
 <!-- end:: Footer -->
