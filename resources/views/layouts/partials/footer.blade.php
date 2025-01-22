35a2ce9304112e8d04c742110c62b603ec3d63ff
Sjoerd Handofsky <sjoerd@webswing.nl>
2025-01-22 14:31:34 +0100

    Added version to footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -4,5 +4,9 @@
 
     </div>
     Copyright &copy; {{ now()->year }} PickZ contributors. All rights reserved
+
+    <div class="float-right d-none d-sm-inline-block">
+        <b>v</b>{{ config('app.version') }}
+    </div>
 </footer>
 <!-- end:: Footer -->
